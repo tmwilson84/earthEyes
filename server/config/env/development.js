97492a7b4e8c22eb2b681ec06184403886613421
Anurag Pal <anurag@ubuntu.(none)>
2014-06-18 09:43:12 -0700

    initial version

---
+++ server/config/env/development.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+    db: 'mongodb://localhost/mean-dev',
+    app: {
+        name: 'MEAN - FullStack JS - Development'
+    },
+    facebook: {
+        clientID: '591584607606867',
+        clientSecret: 'ffc2fc7b63036bd5294dcb9a77f5d9d2',
+        callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    },
+    twitter: {
+        clientID: 'CONSUMER_KEY',
+        clientSecret: 'CONSUMER_SECRET',
+        callbackURL: 'http://localhost:3000/auth/twitter/callback'
+    },
+    github: {
+        clientID: 'APP_ID',
+        clientSecret: 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/github/callback'
+    },
+    google: {
+        clientID: 'APP_ID',
+        clientSecret: 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/google/callback'
+    },
+    linkedin: {
+        clientID: 'API_KEY',
+        clientSecret: 'SECRET_KEY',
+        callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+    }
+};
