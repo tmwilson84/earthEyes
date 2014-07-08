f8c28de117d38cb9e0cb83b53f8f949be72c9604
jewel farazi <jeweljf@gmail.com>
2014-07-08 22:44:00 +0600

    creating new user structure and validation updated because of new require user fields

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,7 +4,8 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-    User = mongoose.model('User');
+    User = mongoose.model('User'),
+    _ = require('lodash');
 
 /**
  * Auth callback
@@ -47,17 +48,27 @@ exports.create = function(req, res, next) {
     user.provider = 'local';
 
     // because we set our user.provider to local our models/user.js validation will always be true
-    req.assert('name', 'You must enter a name').notEmpty();
+    req.assert('first_name', 'You must enter first name').notEmpty();
+    req.assert('last_name', 'You must enter last name').notEmpty();
     req.assert('email', 'You must enter a valid email address').isEmail();
     req.assert('password', 'Password must be between 8-20 characters long').len(8, 20);
     req.assert('username', 'Username cannot be more than 20 characters').len(1,20);
     req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
+    req.assert('dob', 'Please enter a valid date').isDate();
+    req.assert('gender', 'Please select your gender').notEmpty();
 
     var errors = req.validationErrors();
     if (errors) {
         return res.status(400).send(errors);
     }
 
+    user.name.first = req.body.first_name;
+    user.name.last = req.body.last_name;
+
+    user.location.country = req.body.country;
+    user.location.state = req.body.state;
+    user.location.city = req.body.city;
+    
     // Hard coded for now. Will address this with the user permissions system in v0.3.5
     user.roles = ['authenticated'];
     user.save(function(err) {
@@ -73,12 +84,15 @@ exports.create = function(req, res, next) {
 
             return res.status(400);
         }
+        
         req.logIn(user, function(err) {
             if (err) return next(err);
             return res.redirect('/');
         });
+
         res.status(200);
     });
+
 };
 /**
  * Send User
@@ -102,3 +116,22 @@ exports.user = function(req, res, next, id) {
             next();
         });
 };
+
+/**
+ *  Update user
+ */
+exports.update = function(req, res) {
+    // check for logged in user
+    var user = req.user;
+    
+    user = _.extend(user, req.body);
+    
+    user.save(function(err) {
+        if (err) {
+            res.status(400).send('Errors occoured');
+        } else {
+            res.jsonp(user);
+        }
+    });
+
+}
